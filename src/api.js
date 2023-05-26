8a4213edd9362fbfdf0b39ae43fe95781b99e373
Robert Mudzonga <88309711+RobertMudzonga@users.noreply.github.com>
2023-05-26 11:48:19 +0200

    src folder

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "a2a928ef52msh400ef94b26a2370p193fd8jsn89ab17cc1d4a",// enter your rapid api key here
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "a5b4fdb1a30ab832cc0181e4bc793d03"; // enter your key from openweather API
